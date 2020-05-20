3c72774f21d7f18992032455e52e362b81a66a30
VUSAL-PC\VUSAL <alishikhiyev@yahoo.com>
2020-05-20 11:43:03 +0400

    forkme-2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
 @yield('css')
 </head>
 <body>
-	<a href="https://github.com/you"><img style="margin-top: -81px;z-index: 9999 !important;display: block;position: relative;" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_left_red_aa0000.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1"></a>
+	<a href="https://github.com/Alionides/laramedium"><img style="margin-top: -81px;z-index: 9999 !important;display: block;position: relative;" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_left_red_aa0000.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1"></a>
 <!-- Begin Nav
 ================================================== -->
 <nav class="navbar navbar-toggleable-md navbar-light bg-white fixed-top mediumnavigation">
